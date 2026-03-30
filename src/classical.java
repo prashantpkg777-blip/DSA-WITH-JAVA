58d437f5214f10c035f9935597adc2f5e7e6b85f
prashantpkg777-blip <prashantpkg777@gmail.com>
2026-03-30 22:56:16 +0530

    Hello World

---
+++ src/classical.java
@@ -0,0 +1,8 @@
+
+// Classical java method
+
+public class classical {
+    public static void main() {
+        System.out.println("Hello world");
+    }
+}
--- src/first.java
+++
@@ -1,2 +0,0 @@
-public class first {I8WBRFTUET R WG
-}
