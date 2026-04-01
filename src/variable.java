16eab183f97ee1435222bd397c7a95b37b035d0f
prashantpkg777-blip <prashantpkg777@gmail.com>
2026-04-01 20:08:04 +0530

    variables

---
+++ src/variable.java
@@ -0,0 +1,18 @@
+public class variable {
+    static void main() {
+        // inilatize
+        int age = 18;
+        System.out.println(age);
+
+        // case sensitive
+        int weight = 50;
+        int WEIGHT = 60;
+        System.out.println("weight:"+weight);
+        System.out.println("Weight:"+WEIGHT);
+
+        // main is not reversed keyword
+        int main = 5;
+        System.out.println(main);
+
+    }
+}
