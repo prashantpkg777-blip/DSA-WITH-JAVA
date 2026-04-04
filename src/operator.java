b303cf10a0a4b4dbbd858cf73cf316c4a055e48c
prashantpkg777-blip <prashantpkg777@gmail.com>
2026-04-04 23:52:01 +0530

    operator

---
+++ src/operator.java
@@ -0,0 +1,64 @@
+public class operator {
+    static void main() {
+        int a = 25;
+        int b = 6;
+
+        // Arithmetic operator
+        int add = a+b;
+        int sub = a-b;
+        int multi = a*b;
+        int div = a/b;
+        int mod = a%b;
+
+        System.out.println(add);
+        System.out.println(sub);
+        System.out.println(multi);
+        System.out.println(div);
+        System.out.println(mod);
+
+        // Relational operator
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a>=b);
+        System.out.println(a<=b);
+
+        // Logical operator
+        boolean completedDSA = true;
+        boolean completedCore = false;
+
+        System.out.println(completedDSA && completedCore);
+        System.out.println(completedDSA || completedCore);
+        System.out.println(!completedDSA);
+
+        // Assignnment operator
+        int x = 10;
+
+        x += 5;
+        System.out.println(x);
+        x -= 10;
+        System.out.println(x);
+        x *= 6;
+        System.out.println(x);
+        x /= 3;
+        System.out.println(x);
+        x %= 4;
+        System.out.println(x);
+
+        // Unary inc/dec operator
+        int y = 10;
+
+        System.out.println(-y);
+        System.out.println(+y);
+
+        System.out.println(y++);
+        System.out.println(++y);
+
+        System.out.println(y--);
+        System.out.println(--y);
+
+
+
+    }
+}
