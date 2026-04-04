ba181d7a50e4c43a329bb5284995cc3f149dd5f4
prashantpkg777-blip <prashantpkg777@gmail.com>
2026-04-05 00:30:01 +0530

    Input

---
+++ src/TakingInput.java
@@ -0,0 +1,26 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class TakingInput {
+    static void main() {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter first no:");
+        int firstNum = sc.nextInt();
+
+        System.out.println("Enter second no:");
+        int secondNum = sc.nextInt();
+
+        int add = firstNum + secondNum;
+        System.out.println("Total: "+ add);
+
+        BigInteger bg = sc.nextBigInteger();
+        System.out.println("Big Integer: "+ bg);
+
+        boolean flag = sc.nextBoolean();
+        System.out.println(flag);
+
+        sc.close();
+    }
+}
