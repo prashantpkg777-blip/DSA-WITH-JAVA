bc5a7e4754d0cee4216a54fa4c347dcf65059ca4
prashantpkg777-blip <prashantpkg777@gmail.com>
2026-04-06 23:47:32 +0530

    questions

---
+++ src/bestFour.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class bestFour {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+
+        int[] marks = new int[5];
+
+        System.out.println("Enter 5 Subject marks: ");
+        for (int i =0; i<5; i++) {
+            marks[i] = sc.nextInt();
+        }
+
+        int min = marks[0];
+        int sum = marks[0];
+
+        for (int i =1; i<5; i++){
+            sum += marks[i];
+            if (marks[i] < min){
+                min = marks[i];
+            }
+        }
+
+        sum = sum - min;
+
+        double percent = (sum/400.00)*100;
+
+        System.out.println("Droped marks:"+min);
+        System.out.println("Percentage of best of 4 subjects: "+ percent +"%");
+
+    }
+}
---
+++ src/caseChange.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class caseChange {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a Word:");
+        String word = sc.nextLine();
+
+        String lower = word.toLowerCase();
+
+        System.out.println("In LowerCase:" + lower);
+
+        String upper = word.toUpperCase();
+
+        System.out.println("In UpperCase:" + upper);
+
+        sc.close();
+    }
+}
