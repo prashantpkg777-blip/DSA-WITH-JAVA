e514d556b32ab66ef2e7aa580935c199e298602e
prashantpkg777-blip <prashantpkg777@gmail.com>
2026-05-08 00:14:16 +0530

    array problems

--- src/ArrayProblems/ProblemSheet3.java
+++ src/ArrayProblems/ProblemSheet3.java
@@ -25,10 +25,42 @@ public class ProblemSheet3 {
         return arr;
     }
 
+    // Missing number in a range of an array
+    static int missingNumber(int[] arr){
+        int xorSum = 0;
+        for (int n: arr){
+            xorSum = xorSum ^ n;
+        }
+
+        int n = arr.length;
+        for (int i = 0; i <= n; i++){
+            xorSum = xorSum ^ i;
+        }
+        return xorSum;
+    }
+
+    // find unique element
+    static int findUniqueElement(int[] arr){
+        int xorSum = 0;
+        for (int n: arr){
+            xorSum = xorSum ^ n;
+        }
+
+        return xorSum;
+    }
+
     static void main() {
 
         // sort 0s and 1s
         int[] arr = {1,0,0,1,1,0,0,1};
         System.out.println(Arrays.toString(sort0s1s(arr)));
+
+        // missing number
+        int[] arr1 = {1,0,3,2,5};
+        System.out.println(missingNumber(arr1));
+
+        // unique element
+        int[] arr2 = {1,3,5,1,5,6,6};
+        System.out.println(findUniqueElement(arr2));
     }
 }
